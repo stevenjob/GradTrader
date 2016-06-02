d7a5f17dc9b64b9313b004233c4e864d3bdea17e
Steven Job <sjob@scottlogic.co.uk>
2016-06-02 17:01:53 +0100

    Configing gradle

--- backend/src/main/java/gradtrader/App.java
+++ backend/src/main/java/gradtrader/App.java
@@ -8,10 +8,9 @@ import io.dropwizard.setup.Environment;
 
 public class App extends Application<Configuration> {
 
-    public static final String ASSETS_SERVLET_NAME = "assets-servlet";
 
     public static void main(String[] args) throws Exception {
-    	new App().run(new String[]{"server", "config.yml"});
+    	new App().run(args);
     }
 
     @Override
